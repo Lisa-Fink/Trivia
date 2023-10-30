b3377a150cfca16ab572a7f2bd24db5b1d9d4579
lisa fink <lisadfink@gmail.com>
2023-10-30 17:19:14 -0400

    Added function submit category button

--- src/Components/Category.jsx
+++ src/Components/Category.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Category() {
+function Category({ category, setCategory, submitCategory }) {
   const categories = [
     "Geography",
     "Movies",
@@ -10,13 +10,32 @@ function Category() {
     "Random",
   ];
 
-  const categoryButtons = categories.map((category) => (
-    <button>{category}</button>
+  function handleSubmitCategory() {
+    if (category == null) {
+      // TODO: error message
+    } else {
+      submitCategory();
+    }
+  }
+
+  const categoryButtons = categories.map((curCategory, index) => (
+    <button
+      className={category == curCategory ? "selected-btn" : ""}
+      key={index}
+      onClick={() => setCategory(curCategory)}
+    >
+      {curCategory}
+    </button>
   ));
   return (
     <div className="center-col">
       <div className="large-text">Select a category to get started</div>
       <div className="btn-container">{categoryButtons}</div>
+      <div>
+        <button className="wide-btn" onClick={handleSubmitCategory}>
+          Submit
+        </button>
+      </div>
     </div>
   );
 }
--- src/Components/Play.jsx
+++ src/Components/Play.jsx
@@ -1,18 +1,28 @@
 import React, { useState } from "react";
 import Start from "./Start";
 import Category from "./Category";
+import Question from "./Question";
 
 function Play() {
   const [gameState, setGameState] = useState("start");
+  const [category, setCategory] = useState(null);
 
-  return gameState == "start" ? (
+  return gameState == "category" ? (
+    <Category
+      category={category}
+      setCategory={setCategory}
+      submitCategory={() => {
+        setGameState("question");
+      }}
+    />
+  ) : gameState == "question" ? (
+    <Question />
+  ) : (
     <Start
       startGame={() => {
         setGameState("category");
       }}
     />
-  ) : (
-    <Category />
   );
 }
 
