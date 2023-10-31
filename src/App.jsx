46aef2abf8e077ec123eec4fec83cf9bb6c64cf5
lisa fink <lisadfink@gmail.com>
2023-10-31 12:14:51 -0400

    Added instructions page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Components/Header";
 import Play from "./Components/Play";
+import Instructions from "./Components/Instructions";
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <main>
-        <Play />
+        <Routes>
+          <Route path="/" element={<Play />} />
+          <Route path="/play" element={<Play />} />
+          <Route path="/instructions" element={<Instructions />} />
+        </Routes>
       </main>
-    </>
+    </Router>
   );
 }
 
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <header className="flex-row light-text">
       <div className="large-text lower">Trivia App</div>
       <nav className="regular-text flex-row">
-        <a href="#">Play</a>
-        <a href="#">Instructions</a>
+        <Link to="/play">Play</Link>
+        <Link to="/instructions">Instructions</Link>
       </nav>
     </header>
   );
